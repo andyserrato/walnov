2380363cfd0af91c7354f5efcd2eaa0e893b5861
julian14hd <julian14hd@gmail.com>
2017-07-11 20:31:02 +0200

    Card informative cambia dependiendo de la vista.

--- client/app/shared/card-informativo/card-informativo.component.ts
+++ client/app/shared/card-informativo/card-informativo.component.ts
@@ -11,20 +11,33 @@ export class CardInformativoComponent implements OnInit {
   buttonText: string;
   titulo: string;
   parrafos: Array<string>;
+  @Input() view: string;
   @Input() cats: any;
 
   constructor() {
     // de forma momentánea pues no
-    this.showButton = true;
-    this.buttonText = 'Entendido';
-    this.parrafos = new Array<string>();
-    this.parrafos.push('¡Estas creando un wall!');
-    this.parrafos.push('Comienza añadiendo el título y seleccionando una categoría.');
-    this.parrafos.push('Selecciona la imagen que mñas te guste y escribe el inicio de historia.');
-    this.parrafos.push('¡No Olvides invitar a tus amigos y compartirlo en tus redes sociales');
+
+
   }
 
   ngOnInit() {
+
+    this.showButton = true;
+    this.buttonText = 'Entendido';
+    this.parrafos = new Array<string>();
+
+    if(this.view.includes("wall")){
+      this.titulo = '¡Estás creando un Wall!';
+    }
+
+    if(this.view.includes("chat")){
+      this.titulo= '¡Estás creando una Chatstorie!';
+    }
+
+    this.parrafos.push('Comienza añadiendo el título y seleccionando una categoría.');
+    this.parrafos.push('Selecciona la imagen que más te guste y escribe el inicio de historia.');
+    this.parrafos.push('¡No olvides invitar a tus amigos y compartirlo en tus redes sociales!');
+
   }
 
   public entendido () {
