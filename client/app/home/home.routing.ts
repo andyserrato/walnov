9be8bd946f8b7481444eb798934bea639308c7b6
julian14hd <julian14hd@gmail.com>
2017-09-12 14:52:33 +0200

    biblioteca enrutada.

--- client/app/home/home.routing.ts
+++ client/app/home/home.routing.ts
@@ -6,6 +6,9 @@ import { HomeMisChatstoriesComponent } from './home-mis-chatstories/home-mis-cha
 import { HomeMisRelatosComponent } from './home-mis-relatos/home-mis-relatos.component';
 import { HomeRecienteComponent } from './home-reciente/home-reciente.component';
 import { HomeBuscadorComponent } from './home-buscador/home-buscador.component';
+import { GuardadoWallsComponent } from './home-guardado/guardado-walls/guardado-walls.component';
+import { GuardadoRelatosComponent } from './home-guardado/guardado-relatos/guardado-relatos.component';
+import { GuardadoChatstoriesComponent } from './home-guardado/guardado-chatstories/guardado-chatstories.component';
 
 const routes: Routes = [{
   path: 'home',
@@ -14,7 +17,13 @@ const routes: Routes = [{
     { path: 'actividad-reciente', component: HomeRecienteComponent },
     { path: 'mis-relatos', component: HomeMisRelatosComponent },
     { path: 'mis-chatstories', component: HomeMisChatstoriesComponent },
-    { path: 'guardado', component: HomeGuardadoComponent },
+    { path: 'guardado', component: HomeGuardadoComponent, children: [
+        { path: 'walls', component: GuardadoWallsComponent },
+        { path: 'relatos', component: GuardadoRelatosComponent },
+        { path: 'chatstories', component: GuardadoChatstoriesComponent },
+        { path: '', redirectTo: '/home/guardado/walls', pathMatch: 'full'}
+        ]
+    },
     { path: 'mis-walls', redirectTo: '/home/actividad-reciente', pathMatch: 'full'},
     { path: '', redirectTo: '/home/actividad-reciente', pathMatch: 'full'}
 
