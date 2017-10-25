8d6f5da482adc2b5815813135c30aa0eec4bef2f
julian14hd <julian14hd@gmail.com>
2017-10-25 21:30:52 +0200

    landing- part 1

--- client/app/landing/landing.component.ts
+++ client/app/landing/landing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css', '../landing/styles/bootstrap.min.css', '../landing/styles/font-awesome.min.css', '../landing/styles/linearicons.css', '../landing/styles/owl.carousel.css', '../landing/styles/owl.theme.css', '../landing/styles/responsive.css', '../landing/styles/style.css']
+  styleUrls: ['./landing.component.scss', '../landing/styles/bootstrap.min.css', '../landing/styles/font-awesome.min.css', '../landing/styles/linearicons.css', '../landing/styles/owl.carousel.css', '../landing/styles/owl.theme.css', '../landing/styles/responsive.css', '../landing/styles/style.css']
 })
 export class LandingComponent implements OnInit {
 
